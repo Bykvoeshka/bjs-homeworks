df4a6a42dffe8febb063ebb01a047b12d0188ce2
Oleg Sozin <fakemaster0@inbox.ru>
2020-01-19 15:25:00 +0300

    bjs commit

---
+++ 1.1-variables/main.js
@@ -0,0 +1,19 @@
+let name = "Нина Петровна"
+let message = `Привет, мир! Меня зовут ${name}.`
+let algebra = 3;
+let geography = 4;
+let physics = 3;
+
+
+console.log(message);
+let average = (algebra + geography + physics) / 3;
+console.log(average);
+
+console.log("задача №3");
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
